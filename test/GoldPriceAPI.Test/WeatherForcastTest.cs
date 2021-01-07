46e0fc3d47832a12e9c8c1a5176976b897e2ad44
tusharvasani <77061918+tusharvasani@users.noreply.github.com>
2021-01-07 11:20:55 +0800

    Failed Test Case

--- test/GoldPriceAPI.Test/WeatherForcastTest.cs
+++ test/GoldPriceAPI.Test/WeatherForcastTest.cs
@@ -14,7 +14,7 @@ namespace GoldPriceAPI.Test
         [Test]
         public void CheckIDReturnsHongKong()
         {
-            Assert.AreEqual(myCtl.Get(1), "Hong Kong");
+            Assert.AreEqual(myCtl.Get(1), "Hong Kongg");
         }
     }
 }
